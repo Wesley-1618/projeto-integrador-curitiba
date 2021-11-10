c9625bf8a0111cd039a76b737851086baf51f536
W.S.Oliveira <wesleyisilv@gmail.com>
2021-11-10 13:42:51 -0300

    add fetch all on model 'simulacao'

--- src/controller/SimulacaoEmprestimoController.js
+++ src/controller/SimulacaoEmprestimoController.js
@@ -7,5 +7,16 @@ module.exports = {
         const simulacao = await Simulacao.create({cpf_usuario,financeira,renda_mensal,valor_emprestimo,tarifa,parcelas,cet,iof,valor_total});
 
         return res.json(simulacao);
+
+        
+    },
+
+    async fetchall ( req, res ){
+        const {cpf_usuario} = req.query;
+
+        const simulacao = await Simulacao.findAll({ where : {cpf_usuario}});
+
+        return res.json(simulacao);
     }
+    
 };
--- src/routes/routes_SimulacaoEmprestimo.js
+++ src/routes/routes_SimulacaoEmprestimo.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const simulacaoController = require('../controller/SimulacaoEmprestimoController');
 
 routes.post('/simulacao', simulacaoController.store);
+routes.get('/simulacao/all', simulacaoController.fetchall);
 
 module.exports= routes;
