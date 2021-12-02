af62f6ac69c46be75a2b7ca0953a8582537b8dd6
W.S.Oliveira <wesleyisilv@gmail.com>
2021-12-02 17:00:53 -0300

    confirmed controler user loja function

--- src/controller/UsuarioLojaController.js
+++ src/controller/UsuarioLojaController.js
@@ -9,10 +9,10 @@ module.exports = {
         return res.json(usuarioLoja);
     },
 
-    async store(req, res) {
+    async store( req, res ) {
         const {cpf_usuario,cnpj_loja,perfil,data_hora_cadastro} = req.body;
         
-        const usuarioloja = await UsuarioLojaModel.create({cpf_usuario,cnpj_loja,perfil,data_hora_cadastro});
+        const usuarioloja = await UsuarioLojaModel.create({ cpf_usuario,cnpj_loja,perfil,data_hora_cadastro });
 
         return res.json(usuarioloja);
     }
